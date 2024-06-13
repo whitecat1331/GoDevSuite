1d3b94b7d776da269f1ff4ce686710b74a2db10e
whitecat1331 <whitecat1331@gmail.com>
2024-06-13 16:23:37 -0400

    Add misc file for functions without module

---
+++ godevsuite.go
@@ -0,0 +1,13 @@
+package godevsuite
+
+func removeDuplicates[T comparable](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	list := []T{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
